a2db7dfec46484620ee040089cc7f8339a1b7ed0
Barry Biediger <bbiediger@utah.gov>
2017-03-22 14:10:39 -0600

    First commit

---
+++ WMServicesPallet.py
@@ -0,0 +1,14 @@
+from forklift.models import Pallet
+from os import path
+
+class WMServicesPallet(Pallet):
+  def __init__(self):
+     #: this is required to initialize the Pallet base class properties
+     super(MyPallet, self).__init__()
+
+  def build(self, configuration)
+     destination_workspace = path.join(r'W:\\GISServices\\test', "ForkliftTest.gdb")
+     source_workspace = path.join(self.garage, 'Viewer_TrustPub@tla-gis.sde')
+
+     self.add_crate('Counties', {'source_workspace': source_workspace,
+                              'destination_workspace': destination_workspace})
