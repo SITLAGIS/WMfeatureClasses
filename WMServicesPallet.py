e8d700e189629a0050a309ddb37c016b98305534
Barry Biediger <bbiediger@utah.gov>
2017-04-13 11:19:24 -0600

    added crates method with web merc fcs and services.

--- WMServicesPallet.py
+++ WMServicesPallet.py
@@ -6,9 +6,20 @@ class WMServicesPallet(Pallet):
      #: this is required to initialize the Pallet base class properties
      super(WMServicesPallet, self).__init__()
 
+     self.arcgis_services = [('plat/UT_SITLA_CoalPlat', 'MapServer'),
+                             ('plat/UT_SITLA_GeneralMap', 'MapServer'),
+                             ('plat/UT_SITLA_OilGasPlat', 'MapServer'),
+                             ('plat/UT_SITLA_OtherMineralPlat', 'MapServer'),
+                             ('plat/UT_SITLA_SurfacePlat', 'MapServer'),]
+
+     self.webmercservices = 'W:\\GISServices\\WebMercator\\WebMercatorServices.gdb'
+
+     self.copy_data = [self.webmercservices]
+
   def build(self, configuration):
-     destination_workspace = path.join(r'W:\\GISServices\\test', 'ForkliftTest.gdb')
+     #destination_workspace = path.join(r'W:\\GISServices\\WebMercator', 'WebMercatorServices.gdb')
      source_workspace = path.join(self.garage, 'Viewer_TrustPub@tla-gis.sde')
 
-     self.add_crate('Contract_OilShale', {'source_workspace': source_workspace,
-                              'destination_workspace': destination_workspace})
+     self.add_crates(['Contract_Esmt','Contract_Grazing'.'Owner_Coal','Owner_OilGas','Owner_OtherMin',
+                      'Owner_Surface','Trans_Completed'], {'source_workspace': source_workspace,
+                              'destination_workspace': self.webmercservices})
